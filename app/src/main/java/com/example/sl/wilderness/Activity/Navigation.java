d6526389607dffa2ddcacc9edeadff6a99c6f2a7
Simeon Leatherland <simeon.leatherland@gmail.com>
2018-10-04 10:48:24 +0800

    Commit Details: Name - Simeon Leatherland Details - adding the area fragment as well

--- app/src/main/java/com/example/sl/wilderness/Activity/Navigation.java
+++ app/src/main/java/com/example/sl/wilderness/Activity/Navigation.java
@@ -1,8 +1,11 @@
 package com.example.sl.wilderness.Activity;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.sl.wilderness.Fragments.AreaInfo;
+import com.example.sl.wilderness.Fragments.StatusBar;
 import com.example.sl.wilderness.R;
 
 public class Navigation extends AppCompatActivity {
@@ -16,6 +19,24 @@ public class Navigation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation);
+
+        FragmentManager fm = getSupportFragmentManager();
+        //tries to find the fragment by the id of the framelayout in this activity
+        StatusBar sb = (StatusBar)fm.findFragmentById(R.id.status);
+        AreaInfo ai = (AreaInfo)fm.findFragmentById(R.id.area);
+
+        //try find the fragment, if it doesnt exist then create it
+        if(ai == null)
+        {
+            ai = new AreaInfo();
+            fm.beginTransaction().add(R.id.area, ai).commit();
+        }
+        if(sb == null)
+        {
+            sb = new StatusBar();
+            fm.beginTransaction().add(R.id.status, sb).commit();
+        }
+
 
     }
 
--- app/src/main/java/com/example/sl/wilderness/Fragments/AreaInfo.java
+++ app/src/main/java/com/example/sl/wilderness/Fragments/AreaInfo.java
@@ -11,12 +11,12 @@ import android.view.ViewGroup;
 import com.example.sl.wilderness.R;
 
 
-public class StatusBar extends Fragment {
+public class AreaInfo extends Fragment {
 
-
-    public StatusBar() {
+    public AreaInfo() {
         // Required empty public constructor
     }
+
 
 
     @Override
@@ -29,10 +29,7 @@ public class StatusBar extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_status_bar, container, false);
+        return inflater.inflate(R.layout.fragment_area_info, container, false);
     }
-
-
-
 
 }
